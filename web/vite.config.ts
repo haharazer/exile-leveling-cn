f95bbd40ed7f2c8da7f5aa3c81f1935217e3ecc6
haharazer <hanpeng1986@gmail.com>
2025-11-06 09:54:39 +0800

    fix base path error

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/exile-leveling/",
+  base: "/exile-leveling-cn/",
   server: {
     host: true,
   },
